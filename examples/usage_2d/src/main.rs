83cf053bce3a66b40cf0f7be5573d197b2d1e50a
IDEDARY <IDEDARY@gmail.com>
2024-01-19 18:05:53 +0100

    ecs works

--- examples/usage_2d/src/main.rs
+++ examples/usage_2d/src/main.rs
@@ -17,20 +17,27 @@ fn setup(mut commands: Commands) {
     ));
 
     commands.spawn((
-        UiTreeBundle::<NoData, MyWidget>::from( ui() ),
+        // Create new DOM called `MyWidget`
+        UiTreeBundle::<NoData, MyWidget>::from( UiTree::<NoData>::new("MyWidget") ),
+        
+        // Marks the `Transform` to recive data from camera size
         MovableByCamera,
+
     )).with_children(|parent| {
 
+        // Spawn `Root` div
         parent.spawn((
             MyWidget,
             UiLink::path("Root"),
-            layout::Window::FULL.pack(),
+            //                               (20px, 20px)                        (100% - 40px, 100% - 40px)
+            layout::Window::FULL.with_pos( Abs::splat2(20.0) ).with_size( Prc::splat2(100.0) + Abs::splat2(-40.0) ).pack(),
         ));
 
+        // Spawn `Square` div
         parent.spawn((
             MyWidget,
             UiLink::path("Root/Square"),
-            layout::Solid::new().with_align_x(Align::START).pack(),
+            layout::Solid::new().with_align_x(Align::CENTER).pack(),
         ));
 
     });
