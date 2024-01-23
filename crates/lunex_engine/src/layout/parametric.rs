e60c4361b4df407a579d995e456329875bdee019
IDEDARY <IDEDARY@gmail.com>
2024-01-23 17:14:30 +0100

    sync

--- crates/lunex_engine/src/layout/parametric.rs
+++ crates/lunex_engine/src/layout/parametric.rs
@@ -9,41 +9,47 @@ use crate::{NiceDisplay, Align, Layout, NodeSize, DivSize, StackOrientation};
 pub struct Div { // Most basic type, basically every div is List
 
 
-    pub width: DivSize<f32>,
-    pub min_width: Option<NodeSize<f32>>,
-    pub max_width: Option<NodeSize<f32>>,
+    //pub width: DivSize<f32>,
+    //pub min_width: Option<NodeSize<f32>>,
+    //pub max_width: Option<NodeSize<f32>>,
 
-    /// Overwrite dictated alignement from parent's stack
-    pub align_x: Option<Align>,
+    // Overwrite dictated alignement from parent's stack
+    //pub align_x: Option<Align>,
 
 
-    pub height: DivSize<f32>,
-    pub min_height: Option<NodeSize<f32>>,
-    pub max_height: Option<NodeSize<f32>>,
-    pub align_y: Option<Align>,
+    //pub height: DivSize<f32>,
+    //pub min_height: Option<NodeSize<f32>>,
+    //pub max_height: Option<NodeSize<f32>>,
+    //pub align_y: Option<Align>,
 
-    /// Padding distancing border from context
+
+    pub size: NodeSize<Vec2>,
+
+    //Should be computed so NodeData + alternative recursion from bottom to up
+    //pub content_size: Abs (Vec2),
+
+    // Padding distancing border from context
     pub padding: NodeSize<Vec4>,
-    /// Padding used for rendering. 0 by default
-    pub border: NodeSize<Vec4>,
-    /// Padding distancing other divs from border
-    pub margin: NodeSize<Vec4>,
+    // Padding used for rendering. 0 by default
+    //pub border: NodeSize<Vec4>,
+    // Padding distancing other divs from border
+    //pub margin: NodeSize<Vec4>,
 
-    /// If this div breaks the stack?
-    /// Questionable if it shouldn't be a stack limit instead
-    pub breaks: bool,
+    // If this div breaks the stack?
+    // Questionable if it shouldn't be a stack limit instead
+    //pub breaks: bool,
 
 
     // Item positioning
 
     // pub content : Stack || Grid
 
-    pub stack_orientation: StackOrientation,
-    pub stack_gap: NodeSize<f32>,
+    //pub stack_orientation: StackOrientation,
+    //pub stack_gap: NodeSize<f32>,
 
-    pub item_flipped: bool,
-    pub item_next_gap: NodeSize<f32>,
-    pub item_line_gap: NodeSize<f32>,
+    //pub item_flipped: bool,
+    //pub item_next_gap: NodeSize<f32>,
+    //pub item_line_gap: NodeSize<f32>,
 
 
 
