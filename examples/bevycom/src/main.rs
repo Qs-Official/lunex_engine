713fd6ae27601781a3fedbda22a761fc81a683d7
IDEDARY <IDEDARY@gmail.com>
2024-01-24 08:15:52 +0100

    sync

--- examples/bevycom/src/main.rs
+++ examples/bevycom/src/main.rs
@@ -93,6 +93,77 @@ fn setup(
 
     });
 
+    cmd.spawn((
+        UiTreeBundle::<NoData, NoData, MyWidget> {
+            transform: Transform::from_xyz(-300.0, 300.0, 200.0),
+            tree: UiTree::new("MyWidget"),
+            ..default()
+        },
+        msh.add(Mesh::from(shape::Cube { size: 15.0 })),
+        mat.add(Color::rgb(1.0, 0.0, 1.0).into()),
+        Visibility::default(),
+        ViewVisibility::default(),
+
+    )).with_children(|parent| {
+
+        let root = UiLink::path("Root");
+        parent.spawn((
+            MyWidget,
+            root.clone(),
+            Ui::Window::FULL.size(Abs((818.0, 965.0))).pack(),
+            UiMaterial3dBundle::from( mat.add(StandardMaterial { base_color_texture: Some(assets.load("bevycom.png")), alpha_mode: AlphaMode::Blend, unlit: true, ..default() }) ),
+        ));
+
+        let head = root.add("Head");
+        parent.spawn((
+            MyWidget,
+            head.clone(),
+            Ui::Window::new().width(Prc::FULL).height(Abs(168.0)).pack(),
+        ));
+
+        parent.spawn((
+            MyWidget,
+            head.add("Div"),
+            Ui::Div::new().size(Abs::MD).pad(Abs(10.0)).mar(Abs(5.0)).pack(),
+        ));
+
+    });
+
+    cmd.spawn((
+        UiTreeBundle::<NoData, NoData, MyWidget> {
+            transform: Transform::from_xyz(-600.0, 300.0, 400.0),
+            tree: UiTree::new("MyWidget"),
+            ..default()
+        },
+        msh.add(Mesh::from(shape::Cube { size: 15.0 })),
+        mat.add(Color::rgb(1.0, 0.0, 1.0).into()),
+        Visibility::default(),
+        ViewVisibility::default(),
+
+    )).with_children(|parent| {
+
+        let root = UiLink::path("Root");
+        parent.spawn((
+            MyWidget,
+            root.clone(),
+            Ui::Window::FULL.size(Abs((818.0, 965.0))).pack(),
+            UiMaterial3dBundle::from( mat.add(StandardMaterial { base_color_texture: Some(assets.load("bevycom.png")), alpha_mode: AlphaMode::Blend, unlit: true, ..default() }) ),
+        ));
+
+        let head = root.add("Head");
+        parent.spawn((
+            MyWidget,
+            head.clone(),
+            Ui::Window::new().width(Prc::FULL).height(Abs(168.0)).pack(),
+        ));
+
+        parent.spawn((
+            MyWidget,
+            head.add("Div"),
+            Ui::Div::new().size(Abs::MD).pad(Abs(10.0)).mar(Abs(5.0)).pack(),
+        ));
+
+    });
 }
 
 
