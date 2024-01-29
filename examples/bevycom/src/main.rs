691ab191e3bed917b8027f53a503e6ae424cbeca
IDEDARY <IDEDARY@gmail.com>
2024-01-29 12:15:42 +0100

    sync

--- examples/bevycom/src/main.rs
+++ examples/bevycom/src/main.rs
@@ -75,7 +75,7 @@ fn setup(
             MyWidget,
             root.clone(),
             Ui::Window::FULL.size(Abs((818.0, 965.0))).pack(),
-            //UiMaterial3dBundle::from( mat.add(StandardMaterial { base_color_texture: Some(assets.load("bevycom.png")), alpha_mode: AlphaMode::Blend, unlit: true, ..default() }) ),
+            UiMaterial3dBundle::from( mat.add(StandardMaterial { base_color_texture: Some(assets.load("bevycom.png")), alpha_mode: AlphaMode::Blend, unlit: true, ..default() }) ),
         ));
 
         let head = root.add("Head");
@@ -89,22 +89,23 @@ fn setup(
         parent.spawn((
             MyWidget,
             head.add("Icon"),
-            Ui::Div::new().pad(Abs(64.0)).margin_r(Abs(20.0)).br().pack(),
-            //UiMaterial3dBundle::from( mat.add(StandardMaterial { base_color_texture: Some(assets.load("bevycom.png")), alpha_mode: AlphaMode::Blend, unlit: true, ..default() }) ),
+            Ui::Div::new().margin_r(Abs(20.0)).br().pack(),
+            UiContent::new((155.0, 155.0)),
+            UiMaterial3dBundle::from( mat.add(StandardMaterial { base_color_texture: Some(assets.load("bevycom.png")), alpha_mode: AlphaMode::Blend, unlit: true, ..default() }) ),
         ));
 
         parent.spawn((
             MyWidget,
             head.add("Rank"),
-            Ui::Div::new().margin_b(Abs(20.0)).pack(),
-            UiContent::new((100.0, 20.0))
+            Ui::Div::new().margin_b(Abs(10.0)).pack(),
+            UiContent::new((100.0, 30.0))
         ));
 
         parent.spawn((
             MyWidget,
             head.add("Name"),
             Ui::Div::new().margin_b(Abs(20.0)).pack(),
-            UiContent::new((350.0, 40.0))
+            UiContent::new((350.0, 45.0))
         ));
 
         let list = head.add("List");
@@ -120,21 +121,21 @@ fn setup(
                 MyWidget,
                 list.add("Missions"),
                 Ui::Div::new().pack(),
-                UiContent::new((200.0, 20.0))
+                UiContent::new((200.0, 30.0))
             ));
 
             parent.spawn((
                 MyWidget,
                 list.add("Kills"),
                 Ui::Div::new().pack(),
-                UiContent::new((200.0, 20.0))
+                UiContent::new((150.0, 30.0))
             ));
 
             parent.spawn((
                 MyWidget,
                 list.add("Status"),
                 Ui::Div::new().pack(),
-                UiContent::new((200.0, 20.0))
+                UiContent::new((250.0, 30.0))
             ));
         }
 
